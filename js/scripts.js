06832481bfd2851e2969aaa21d0931009cb24518
colton lacey <coltnutmiles14@gmail.com>
2019-04-12 11:04:05 -0700

    add html content and add objects in js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Business logic
+function PizzaOrderForm(){
+  this.pizzas = [],
+  this.currentId = 0
+}
+
+function Pizza(size, topping1, topping2, topping3){
+  this.size = size,
+  this.toppings = {
+    topping1: topping1,
+    topping2: topping2,
+    topping3: topping3
+  }
+}
