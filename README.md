d0a681d2b5dee16a23e09e1c1385c330bc1f3de8
colton lacey <coltnutmiles14@gmail.com>
2019-04-13 08:40:15 -0700

    display the pizza the user ordered plus their price

---
+++ README.md
@@ -0,0 +1,39 @@
+#  Beep!Boop!
+
+## https://github.com/Colton141/week-3-friday-project/tree/gh-pages
+
+#### breaks down numbers and replace them with beep boop.
+
+#### By Colton Lacey
+
+## Description
+
+This program takes a number that the user has inputed and it down from 0 to the number to user put. Then it takes ever number that has a 3 in it and replaces that number in the sting of number with "I'm sorry Dave. I'm afraid I can't do that!". It then takes every number that has a 2 in it and replaces that number in the string with "Boop!". it will also then take every number that has a 1 in it and replaces that number with "Beep!". Last it takes this new string and displays it back to the user.
+
+## Spec Sheet
+
+| Behavior      |      Input         | Output |
+| ------------- |:-------------:     | -----:|
+| The program returns a range of numbers from 0 to the users inputted number     | 4     | 0,2,3,4 |
+| The program will take every number that has a 3 in it and returns "I'm sorry Dave. I'm afraid I can't do that"| 3 |   1, 2, I'm sorry Dave.... |
+| The program takes ever number in the range that has a 2 in it and returns "Boop!" for the whole number | 2   |  1, Boop! |
+|The prgram takes every number in the range that has a 1 in it and returns "Beep!" for the whole number| 3 | Beep!, Boop!, I'm sorry Dave... |
+
+## Setup/Installation Requirements
+
+* Clone down this repo to your local files
+* Open the files in your folder
+* Open the index.html file in your local browser
+* enter in your number and see your results
+
+
+## Technologies Used
+
+* HTML
+* CSS
+* Bootstrap
+* JavaScript
+* jquery
+
+
+Copyright (c) 2019 Colton Lacey
--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,68 @@ function PizzaOrderForm(){
   this.currentId = 0
 }
 
+
+PizzaOrderForm.prototype.addPizza = function(pizza){
+  pizza.id = this.assignId();
+  this.pizzas.push(pizza);
+}
+PizzaOrderForm.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+
 function Pizza(size, topping1, topping2, topping3){
   this.size = size,
-  this.toppings = {
-    topping1: topping1,
-    topping2: topping2,
-    topping3: topping3
-  }
+  this.topping1 = topping1,
+  this.topping2 = topping2,
+  this.topping3 = topping3
 }
-
-PizzaOrderForm.prototype.Addpizza = function(){
-  pizza.id = this.assignId();
-  this.pizzas.push(pizza)
-}
-
-
-
-
 //User interface
+var pizzaOrderForm = new PizzaOrderForm();
+
+function DisplayPizzaOrder(orderForm){
+  var topping1Display = "";
+  var topping2Display = "";
+  var topping3Display = "";
+  var sizeDisplay = "";
+  orderForm.pizzas.forEach(function(pizza){
+    topping1Display += pizza.topping1;
+    topping2Display += pizza.topping2;
+    topping3Display += pizza.topping3;
+    sizeDisplay += pizza.size;
+  })
+
+  $("#cheese").html(topping1Display);
+  $("#sauce").html(topping2Display);
+  $("#topping").html(topping3Display);
+  if (sizeDisplay === "1") {
+    $("#total").html("$17.95");
+  } else if (sizeDisplay === "2") {
+    $("#total").html("$15.45");
+  } else if (sizeDisplay === "3") {
+    $("#total").html("$12.20");
+  }
+};
+
+
+
+
+
+
+
 $("document").ready(function(){
-  event.preventDefault();
+  $("form#order-form").submit(function(event){
+    event.preventDefault();
+  var size = $("#pizza-size").val();
+  var topping1 = $("#topping1").val();
+  var topping2 = $("#topping2").val();
+  var topping3 = $("#topping3").val();
+  console.log(size);
+  var newPizza = new Pizza(size, topping1, topping2, topping3);
+  pizzaOrderForm.addPizza(newPizza);
+
+  DisplayPizzaOrder(pizzaOrderForm);
+
+})
 })
