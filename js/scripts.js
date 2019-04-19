bb29974dc8dbae18b0323801d54d9310e4e87fab
colton lacey <coltnutmiles14@gmail.com>
2019-04-19 12:25:10 -0700

    clean up order form

--- js/scripts.js
+++ js/scripts.js
@@ -32,25 +32,43 @@ function DisplayPizzaOrder(orderForm){
   var topping3Display = "";
   var sizeDisplay = "";
   orderForm.pizzas.forEach(function(pizza){
-    topping1Display += pizza.topping1;
-    topping2Display += pizza.topping2;
-    topping3Display += pizza.topping3;
-    sizeDisplay += pizza.size;
+    topping1Display = pizza.topping1;
+    topping2Display = pizza.topping2;
+    topping3Display = pizza.topping3;
+    sizeDisplay = pizza.size;
   })
-
-  $("#cheese").html(topping1Display);
-  $("#sauce").html(topping2Display);
-  $("#topping").html(topping3Display);
+  $("#cheese").text(topping1Display);
+  $("#sauce").text(topping2Display);
+  $("#topping").text(topping3Display);
   if (sizeDisplay === "1") {
-    $("#total").html("$17.95");
+    $("#size").text("Large");
+    $("#total").text("$17.95");
   } else if (sizeDisplay === "2") {
-    $("#total").html("$15.45");
+    $("#size").text("Medium");
+    $("#total").text("$15.45");
   } else if (sizeDisplay === "3") {
-    $("#total").html("$12.20");
+    $("#size").text("Small");
+    $("#total").text("$12.20");
   }
 };
+//This fuunction clears out the order form
+function newPizza(){
+  $("button#new-pizza").on("click", function(){
+    pizzaOrderForm.pizzas.splice(0, 5);
+    var topping1Display = "";
+    var topping2Display = "";
+    var topping3Display = "";
+    var sizeDisplay = "";
+    $("#cheese").html(topping1Display);
+    $("#sauce").html(topping2Display);
+    $("#topping").html(topping3Display);
+    $("#size").html(sizeDisplay);
+    $("#total").html("0")
+  })
+}
 
 $("document").ready(function(){
+  newPizza();
   $("form#order-form").submit(function(event){
     event.preventDefault();
   var size = $("#pizza-size").val();
